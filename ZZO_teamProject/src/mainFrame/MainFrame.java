09921787e92aa0bad1d1a95801ddf88623dd0710
Insang Wab Jo <dhkq92@icloud.com>
2017-03-17 09:51:11 +0900

    17.03.17

--- ZZO_teamProject/src/mainFrame/MainFrame.java
+++ ZZO_teamProject/src/mainFrame/MainFrame.java
@@ -20,6 +20,7 @@ public class MainFrame extends JFrame {
 
   private JPanel contentPane;
   private JLabel lblUserStat;
+  private Color colMenu;
 
   /**
    * Launch the application.
@@ -46,69 +47,76 @@ public class MainFrame extends JFrame {
     setBounds(100, 100, 1000, 650);
     setResizable(false);
     contentPane = new JPanel();
-    contentPane.setBackground(new Color(90, 220, 230));
+    contentPane.setBackground(new Color(244, 188, 192));
     contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
     setContentPane(contentPane);
     contentPane.setLayout(null);
 
-    JPanel leftPanel = new JPanel();
-    leftPanel.setBackground(new Color(180, 220, 250));
-    leftPanel.setBounds(6, 6, 365, 266);
-    contentPane.add(leftPanel);
-    leftPanel.setLayout(null);
+    colMenu = new Color(240, 210, 210);
+
+    JPanel selectPanel = new JPanel();
+    selectPanel.setBackground(colMenu);
+    selectPanel.setBounds(6, 6, 168, 266);
+    contentPane.add(selectPanel);
+    selectPanel.setLayout(null);
 
     JComboBox cboMenu = new JComboBox();
     cboMenu.setModel(new DefaultComboBoxModel(new String[] { "부품을 선택해주세요", "CPU", "메인보드", "그래픽카드", "메모리카드" }));
-    cboMenu.setBounds(6, 6, 147, 32);
-    leftPanel.add(cboMenu);
+    cboMenu.setBounds(6, 6, 156, 32);
+    selectPanel.add(cboMenu);
 
     JScrollPane scrollPane = new JScrollPane();
-    scrollPane.setBounds(6, 49, 147, 206);
-    leftPanel.add(scrollPane);
+    scrollPane.setBounds(6, 49, 156, 206);
+    selectPanel.add(scrollPane);
 
     JList list = new JList();
     scrollPane.setViewportView(list);
 
-    JPanel centerPanel = new JPanel();
-    centerPanel.setBackground(new Color(180, 220, 250));
-    centerPanel.setBounds(383, 6, 611, 616);
-    contentPane.add(centerPanel);
+    JPanel detailPanel = new JPanel();
+    detailPanel.setBackground(colMenu);
+    detailPanel.setBounds(383, 6, 611, 616);
+    contentPane.add(detailPanel);
 
     JLabel lblNewLabel = new JLabel("제품 상세페이지");
-    centerPanel.add(lblNewLabel);
+    detailPanel.add(lblNewLabel);
 
-    JPanel panel = new JPanel();
-    panel.setBackground(new Color(180, 220, 250));
-    panel.setBounds(6, 284, 367, 338);
-    contentPane.add(panel);
-    panel.setLayout(null);
+    JPanel boxpanel = new JPanel();
+    boxpanel.setBackground(colMenu);
+    boxpanel.setBounds(6, 276, 373, 346);
+    contentPane.add(boxpanel);
+    boxpanel.setLayout(null);
 
     lblUserStat = new JLabel("조인상님! 어서오세요!");
     lblUserStat.setFont(new Font("consolas", Font.BOLD, 20));
     lblUserStat.setHorizontalAlignment(SwingConstants.CENTER);
     lblUserStat.setBounds(6, 6, 355, 30);
-    panel.add(lblUserStat);
+    boxpanel.add(lblUserStat);
 
-    JLabel lblNewLabel_1 = new JLabel("장바구니 목록");
+    JLabel lblNewLabel_1 = new JLabel("현재 장바구니 목록");
     lblNewLabel_1.setFont(new Font("consolas", Font.PLAIN, 15));
     lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
     lblNewLabel_1.setBounds(6, 35, 355, 16);
-    panel.add(lblNewLabel_1);
+    boxpanel.add(lblNewLabel_1);
 
     JScrollPane scrollPane_1 = new JScrollPane();
     scrollPane_1.setBounds(78, 63, 283, 229);
-    panel.add(scrollPane_1);
+    boxpanel.add(scrollPane_1);
 
     JButton btnNewButton = new JButton("완료");
     btnNewButton.setBounds(279, 303, 82, 29);
-    panel.add(btnNewButton);
+    boxpanel.add(btnNewButton);
 
     JButton button = new JButton("초기화");
     button.setBounds(185, 303, 82, 29);
-    panel.add(button);
+    boxpanel.add(button);
 
-    JButton button_1 = new JButton("장바구니...");
+    JButton button_1 = new JButton("장바구니");
     button_1.setBounds(78, 303, 88, 29);
-    panel.add(button_1);
+    boxpanel.add(button_1);
+
+    JPanel optionPanel = new JPanel();
+    optionPanel.setBackground(colMenu);
+    optionPanel.setBounds(179, 6, 200, 266);
+    contentPane.add(optionPanel);
   }
 }
