105d563f1b5e8a4699b0ccedd7752760146a5958
Insang Wab Jo <dhkq92@icloud.com>
2017-03-20 16:51:37 +0900

    asdf

--- mrhiProject/src/sangjin/DB/DB.java
+++ mrhiProject/src/sangjin/DB/DB.java
@@ -6,19 +6,20 @@ import java.sql.DriverManager;
 import java.util.Properties;
 
 public class DB {
-	public static Connection comCon(){
-		Connection conn=null;
-		try {
-			FileInputStream fis=new FileInputStream("d:\\comnawa.prop");
-			Properties prop = new Properties();
-			prop.load(fis);
-			String url=prop.getProperty("url");
-			String id=prop.getProperty("id");
-			String password=prop.getProperty("password");
-			conn = DriverManager.getConnection(url, id, password);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return conn;
-	}
+  public static Connection comCon() {
+    Connection conn = null;
+    try {
+      String a = (String.valueOf(DB.class.getResource("comnawa.prop"))).replaceAll("file:", "");
+      FileInputStream fis = new FileInputStream(String.valueOf(a));
+      Properties prop = new Properties();
+      prop.load(fis);
+      String url = prop.getProperty("url");
+      String id = prop.getProperty("id");
+      String password = prop.getProperty("password");
+      conn = DriverManager.getConnection(url, id, password);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return conn;
+  }
 }
