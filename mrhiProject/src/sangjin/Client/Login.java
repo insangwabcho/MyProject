7ce4757494f920fdc328dfac396f42a4006360b1
insang wab jo <dhkq92@icloud.com>
2017-03-24 11:44:21 +0900

    asdf

--- mrhiProject/src/sangjin/Client/Login.java
+++ mrhiProject/src/sangjin/Client/Login.java
@@ -172,7 +172,7 @@ public class Login extends JFrame {
     contentPane.add(lblNewLabel_1);
 
     //ImageIcon tmplogo = new ImageIcon("d:\\comnawa\\comnawalogo.png");
-    String a = (String.valueOf(DB.class.getResource("img/comnawalogo.png"))).replaceAll("file:", "");
+    String a = (String.valueOf(sungwon.DB.DB.class.getResource("img/comnawalogo.png"))).replaceAll("file:", "");
     ImageIcon tmplogo = new ImageIcon(a);
 
     try {
