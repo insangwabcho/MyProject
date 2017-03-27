7ef38a08bbb04e4d086bf8a5caf94419fb77dd95
Diaspora <Diaspora@DESKTOP-000AKBN>
2017-03-27 10:04:09 +0900

    sdf

--- mrhiProject/src/kwanwoo/MultiClient.java
+++ mrhiProject/src/kwanwoo/MultiClient.java
@@ -17,7 +17,7 @@ public class MultiClient {
 			System.out.println("서버와 연결이 되었습니다.");
 		//사용자로부터 얻은 문자열을 서버로 전송해주는 역할을 하는 thread
 			Thread sender = new Sender(socket,sname);
-		//서버에서 보내는 메시지를 사용자의 콘솔에 출력하는 스레드.
+		//서버에서 보내는 메시지를 사용자의 콘솔에 출력하는 스레드.//
 			Thread receiver = new Receiver(socket);
 			System.out.println("채팅방에 입장하셨습니다.");
 			sender.start();
